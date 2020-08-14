a9e3a29a1e42983a8771d926828d48cbed3cca9d
Daniel Pellicano <danielwebmaster1@gmail.com>
2020-08-14 11:54:53 -0300

    database insert classes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/user', (request, response) => {
-  return response.json('Hellow Word');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
